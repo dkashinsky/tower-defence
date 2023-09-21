cebcd27882518bc3dce2fa053c722e4ed785f035
Dmitry Kashinsky <38276954+dkashinsky@users.noreply.github.com>
2023-09-21 12:29:28 +1200

    Add help scene

---
+++ Assets/Scripts/TowerBasement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TowerBasement : MonoBehaviour
+{
+    public void OnMouseDown()
+    {
+        Debug.Log("Tree clicked!");
+    }
+}
